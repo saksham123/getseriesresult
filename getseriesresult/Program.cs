eb691e5f4e7258c8059c5bbf4e97aab9aba382c6
saksham rana <0blackbuck@gmail.com>
2019-01-27 19:45:54 -0500

    Add project files.

---
+++ getseriesresult/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace getseriesresult
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
